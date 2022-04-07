5b02063573aca188124f92fcc92f8fd05c3df0ba
benuserxxxx <benuserxxx@gmail.com>
2022-04-06 20:52:10 -0500

    diseno c

--- app/Http/Livewire/Estudiantes.php
+++ app/Http/Livewire/Estudiantes.php
@@ -6,6 +6,7 @@ use Livewire\Component;
 use App\Models\User;
 use App\Models\Nivel;
 use App\Models\Total_puntos;
+use App\Models\Practica;
 
 class Estudiantes extends Component
 {
@@ -16,15 +17,73 @@ class Estudiantes extends Component
         $grados = Nivel::get();
         $estudiantes = null;
         $puntajes = '[]';
+        $dias = [
+            0 => [], //lunes
+            1 => [],
+            2 => [],
+            3 => [],
+            4 => []
+        ];
+
+        $estudiantes_data = [];
 
         if($this->gradoseleccionado!=null){
+            
+            $from = date('Y/m/d',strtotime('this week'));
+            $to = date('Y/m/d',strtotime('this week +4 days'));
+            //$from = date('d/m/Y',strtotime('03/04/2022'));
+            //$to = date('d/m/Y',strtotime('07/04/2022'));
+
             $estudiantes = User::where(['nivel_id'=>$this->gradoseleccionado])->orderBy('apellido','asc')->get();
-            $puntajes = Total_puntos::where(['grado_id'=>$this->gradoseleccionado])->get()->toArray();
+            $puntajes = Total_puntos::where(['grado_id'=>$this->gradoseleccionado])->whereBetween('created_at',[$from,$to])->get();
+            $practicas = Practica::whereHas('grados', function($query){
+                $query->where(['nivel_id'=>$this->gradoseleccionado]);
+            })->get();
+
+            
+
+            if($estudiantes->count()>0){
+                foreach($estudiantes as $estudiante){
+                    $estudiantes_data[$estudiante->id] = [];
+                }
+            }
+
+            if($practicas->count()>0){
+                foreach($practicas as $practica){
+                    $dias2 = json_decode($practica->dias,true);
+                    if (is_array($dias2) && count($dias2)>0) {
+                        foreach ($dias2 as $dia) {
+                            $dias[$dia][$practica->id] = $practica->titulo;
+                        }
+                    }
+                }
+            }
+
+            
+
+            if($puntajes->count()>0){
+                foreach($puntajes as $puntaje){
+                    $estudiante_id = $puntaje->user_id;
+                    
+                    $notas = json_decode($puntaje->puntos,true);
+                    if(is_array($notas) && count($notas)>0){
+                        foreach ($notas as $notav) {
+                            $dayoftheweek = date('w',strtotime($notav['fecha'])) - 1;
+                            $nota = $notav['nota'];
+                            $estudiantes_data[$estudiante_id][$dayoftheweek][$puntaje->tarea_id] = $nota;
+                        }
+                    }
+                }
+            }
+
+            
         }
         return view('livewire.estudiantes',[
             'grados'=>$grados,
             'estudiantes'=>$estudiantes,
             'puntajes'=>$puntajes,
+            'dias'=>$dias,
+            'estudiantes_data'=>$estudiantes_data
         ]);
     }
 }
--- resources/views/livewire/estudiantes.blade.php
+++ resources/views/livewire/estudiantes.blade.php
@@ -8,16 +8,24 @@
             @endforeach
         </x-select>
         <hr>
+        @php
+            $textdias = ['Lunes','Martes','Miercoles','Jueves','Viernes'];
+        @endphp
         <x-table-responsive>
             <thead>
                 <tr>
-                    <x-th value="N°" />
-                    <x-th value="Estudiante" />
-                    <x-th value="Lunes" />
-                    <x-th value="Martes" />
-                    <x-th value="Miercoles" />
-                    <x-th value="Jueves" />
-                    <x-th value="Viernes" />
+                    <x-th rowspan="2" value="N°" />
+                    <x-th rowspan="2" value="Estudiante" />
+                    @foreach ($dias as $index => $dia)
+                    <x-th colspan="{{count($dia)}}" value="{{$textdias[$index]}}" />
+                    @endforeach
+                </tr>
+                <tr>
+                    @foreach ($dias as $dia)
+                        @foreach ($dia as $lapractica)
+                        <x-th style="height: 150px" class="-rotate-90	" value="{{$lapractica}}" />
+                        @endforeach
+                    @endforeach
                 </tr>
             </thead>
             <tbody>
@@ -26,18 +34,33 @@
                     <tr>
                         <x-td>{{$indexe+1}}</x-td>
                         <x-td>{{$estudiante->apellido . ', ' . $estudiante->name}}</x-td>
-                        <x-td></x-td>
-                        <x-td></x-td>
-                        <x-td></x-td>
-                        <x-td></x-td>
-                        <x-td></x-td>
-                        <x-td></x-td>
+                        
+                        @foreach ($dias as $index1 => $dia2)
+                            @foreach ($dia2 as $index2 => $lapractica2)
+                            <x-td>
+                                @if(array_key_exists($estudiante->id,$estudiantes_data))
+                                    @if(array_key_exists($index1,$estudiantes_data[$estudiante->id]))
+                                        @if(array_key_exists($index2,$estudiantes_data[$estudiante->id][$index1]))
+                            {{$estudiantes_data[$estudiante->id][$index1][$index2]}}
+
+                                        @else
+                                            - 
+                                        @endif
+                                    @else
+                                        - 
+                                    @endif
+                                @else
+                                    -
+                                @endif
+                            </x-td>
+                            @endforeach
+                        @endforeach
+                        
                     </tr>        
                     @endforeach
                 @endif
                 
             </tbody>
         </x-table-responsive>
-        {{print_r($puntajes)}}
     </x-panel>
 </div>
