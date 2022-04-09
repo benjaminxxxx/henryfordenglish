30424f0575f327e7e470292b84b90b45a1ad70eb
benuserxxxx <benuserxxx@gmail.com>
2022-04-09 12:59:40 -0500

    alternativas

--- resources/views/practica/jugar.blade.php
+++ resources/views/practica/jugar.blade.php
@@ -26,6 +26,52 @@
     .bg-box {
         max-width: 88% !important;
     }
+    
+.good .checks{
+    color: #008A22;
+}
+.good span.bol,
+.bad span.bol{
+    display: inline-block;
+    width: 40px;
+    height: 40px;
+    
+    border-radius: 50%;
+    overflow: hidden;
+    text-align: center;
+    line-height: 39px;
+    margin-right: 16px;
+}
+.bad span.bol{
+    background: #ff9d9d;
+    fill: #e41b1b;
+}
+.good span.bol{
+    background: #BFE2C8;
+    fill: green;
+}
+.good span.bol svg,
+.bad span.bol svg{
+    display:inline-block;
+    width: 16px;
+    height: 16px;
+}
+.good .success-text{
+    color: #008A22;
+    font-weight: bold;
+    font-size: 20px;
+}
+.bad .error-text{
+    color: rgb(228, 27, 27);
+    font-weight: bold;
+    font-size: 20px;
+}
+.tiitle_correct_answer{
+    font-size: 20px;
+}
+.bad .checks{
+    color: rgb(228, 27, 27);
+}
     @media(max-width:760px){
         .bg-box {
             max-width: 100% !important;
@@ -224,8 +270,37 @@
                 str_img = '<img src="'+link+'" style="max-height:200px" />';
             }
         }
+
+        var correct_answer_result = '<div class="good" style="display:none">';
+        var iconcheck = '<span class="icon bol"><svg xmlns="http://www.w3.org/2000/svg" width="23px" height="23px" viewBox="0 0 46 46"> <path fill-rule="evenodd" d="M44.596 3.723L40.546.871a2.399 2.399 0 0 0-3.403.666l-19.85 30.33-9.123-9.45a2.386 2.386 0 0 0-3.459 0l-3.464 3.588c-.953.99-.953 2.596 0 3.593l14.024 14.536c.784.813 2.022 1.435 3.133 1.435 1.11 0 2.234-.722 2.953-1.81l23.89-36.52c.758-1.149.47-2.732-.651-3.516z"></path> </svg> </span>';
+        var iconcheckb = '<span class="icon bol"><svg width="23px" height="23px" viewBox="0 0 120 120" version="1.1" xmlns="http://www.w3.org/2000/svg" xmlns:xlink="http://www.w3.org/1999/xlink"> <defs></defs> <g id="Artboard" stroke-width="1" fill-rule="evenodd"> <path d="M95.3975981,16.7092532 C94.4507975,15.7635823 92.9157403,15.7635823 91.9689398,16.7092532 L61.7148883,46.9699486 C60.7680877,47.9156195 59.2330306,47.9156195 58.28623,46.9699486 L28.0310602,16.7092532 C27.0842597,15.7635823 25.5492025,15.7635823 24.6024019,16.7092532 L16.7095545,24.6028135 C15.7634818,25.5505349 15.7634818,27.0841609 16.7095545,28.0318824 L46.9658425,58.2847591 C47.9163838,59.2308247 47.9163838,60.7677624 46.9658425,61.713828 L16.7095545,91.9756404 C15.7634818,92.9233618 15.7634818,94.4569878 16.7095545,95.4047093 L24.6024019,103.290451 C25.5492025,104.236122 27.0842597,104.236122 28.0310602,103.290451 L58.28623,73.0364571 C58.7395785,72.5798506 59.3567356,72.3230103 60.0005591,72.3230103 C60.6443827,72.3230103 61.2615398,72.5798506 61.7148883,73.0364571 L91.9689398,103.290451 C92.919481,104.236516 94.4515299,104.236516 95.3975981,103.290451 L103.290446,95.4024753 C104.236518,94.4547539 104.236518,92.9211279 103.290446,91.9734064 L73.0363941,61.7160619 C72.0903214,60.7683404 72.0903214,59.2347145 73.0363941,58.286993 L103.290446,28.0318824 C104.236518,27.0841609 104.236518,25.5505349 103.290446,24.6028135 L95.3975981,16.7103702 L95.3975981,16.7092532 Z" id="Shape" fill-rule="nonzero"></path> </g> </svg> </span>';
+            correct_answer_result+='<div class="flex items-center success-text">'+iconcheck+' Correcto</div></div>';
+        
+            var incorrect_answer_result2='<p class="checks"><div class="flex items-center error-text">'+iconcheckb+' Incorrecto</div></p>';
+
+        var incorrect_answer_result = '<div class="bad" style="display:none">'+incorrect_answer_result2+'<p class="tiitle_correct_answer ">Respuesta correcta</p>';
+            
+        var arranswer = dataquestion.answer.split('|');
+        for (let ca = 0; ca < arranswer.length; ca++) {
+            const element = arranswer[ca];
+            
+            var is_img = element.split('.').pop();
+
+            if(is_img=='png' || is_img=='jpg'){
+                
+                var linkr = element;
+
+                incorrect_answer_result+='<p class="notranslate"><img src="'+linkr+'" style="max-height:50px" /></p>';
+            }else{
+                incorrect_answer_result+='<p class="notranslate text-2xl">'+element+'</p>';
+            }
+        }
+        
+        incorrect_answer_result+='</div>';
+        
         question += str_img;
         question += '<div class="'+cssforflex+'">' + textanswer + '</div>';
+        question += '<div class="results col-span-1">'+correct_answer_result+incorrect_answer_result+'</div>';
         question += btn;
         question += '</form>';
         $('.questions').html(question);
@@ -320,8 +395,11 @@
                         }
                         
                         document.getElementById('correcto').play();
+                        $('.good').show();
+                    }else{
+                       
+                        $('.bad').show();
                     }
-                    
                     /*
                     if(answer.toUpperCase()==myanswer){
                         if(points!=null){
